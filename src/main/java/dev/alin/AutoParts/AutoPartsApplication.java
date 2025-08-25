469e0edb9fa4dcf6f9f97a0cc7dcdefa1e952c30
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-08-25 13:40:37 +0300

    Added home_page.html

--- src/main/java/dev/alin/AutoParts/AutoPartsApplication.java
+++ src/main/java/dev/alin/AutoParts/AutoPartsApplication.java
@@ -2,12 +2,21 @@ package dev.alin.AutoParts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class AutoPartsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoPartsApplication.class, args);
 	}
+
+    @RequestMapping("/")
+    public String getMainPage(Model model) {
+        return "parts_page";
+    }
 
 }
