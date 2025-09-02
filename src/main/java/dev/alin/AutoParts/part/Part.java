dc8b53b8b6d19445d3ded8a84b4913c2898d4bd1
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-09-02 09:19:33 +0300

    changed annotations and removed unused methods

--- src/main/java/dev/alin/AutoParts/part/Part.java
+++ src/main/java/dev/alin/AutoParts/part/Part.java
@@ -1,9 +1,7 @@
 package dev.alin.AutoParts.part;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import dev.alin.AutoParts.manufacturer.Manufacturer;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 
@@ -14,25 +12,16 @@ import lombok.Getter;
 public class Part {
 
     @Id
-    @Column(name = "part_id")
     private long id;
-    @Column(name = "part_name")
+    @Column()
     private String name;
-    @Column(name = "part_manufacturer")
-    private String manufacturer;
-    @Column(name = "part_description")
+    @ManyToOne
+    @JoinColumn(name = "manufacturer_id", nullable = false)
+    private Manufacturer manufacturer;
+    @Column()
     private String description;
-    @Column(name = "part_price")
+    @Column()
     private int price;
 
     public Part() {}
-
-    public Part(long id, String name, String manufacturer, String description, int price) {
-        this.id = id;
-        this.name = name;
-        this.manufacturer = manufacturer;
-        this.description = description;
-        this.price = price;
-    }
-
 }
