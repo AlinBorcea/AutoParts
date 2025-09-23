e375a0c11f545616b31d7aeee7ec3d25e21efbf8
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-09-23 08:29:26 +0300

    removed unused imports and variable

--- src/main/java/dev/alin/AutoParts/part/PartController.java
+++ src/main/java/dev/alin/AutoParts/part/PartController.java
@@ -1,18 +1,11 @@
 package dev.alin.AutoParts.part;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Controller
 public class PartController {
-
-    @Autowired
-    private PartService service;
 
     @RequestMapping("/part")
     public String getPartPage(Model model) {
